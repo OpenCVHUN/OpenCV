e2e9b87f17453476b448e59bbed321ab7ec8072f
tripod <paulikdani@gmail.com>
2018-03-09 16:39:37 +0100

    Segítő függvények
    Egyelőre csak egy imshow resizer és egy időmérő, később lehet bővíteni...

---
+++ OpenCVHungary/helpers.cpp
@@ -0,0 +1,59 @@
+﻿#include <opencv2/opencv.hpp>
+#include <wtypes.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <chrono>
+#include "helpers.h"
+
+void cv::imshowResized(const cv::String& winname, const cv::Mat& img) {
+	RECT desktop;
+	const HWND hDesktop = GetDesktopWindow();
+	GetWindowRect(hDesktop, &desktop);
+	double screenWidth = desktop.right;
+	double screenHeight = desktop.bottom;
+	int horRatio, verRatio, zoomRatio;
+
+	if (screenWidth < img.cols || screenHeight < img.rows) {
+		horRatio = ceil(img.cols / screenWidth);
+		verRatio = ceil(img.rows / screenHeight);
+	}
+	else {
+		horRatio, verRatio = 1;
+	}
+	zoomRatio = max(horRatio, verRatio);
+
+	cv::namedWindow(winname, cv::WINDOW_NORMAL);
+	cv::resizeWindow(winname, img.cols / zoomRatio, img.rows / zoomRatio);
+	cv::imshow(winname, img);
+}
+
+Timer::Timer() : startTime(std::chrono::high_resolution_clock::now()), middleTime(std::chrono::high_resolution_clock::now()) {}
+
+cv::String Timer::midTime() {
+	std::chrono::duration<double, std::milli> midDuration(std::chrono::high_resolution_clock::now() - middleTime);
+	std::ostringstream ostr;
+	ostr << std::fixed << std::setprecision(2) << midDuration.count();
+	middleTime = std::chrono::high_resolution_clock::now();
+
+	return "(" + ostr.str() + " ms)";
+}
+
+void Timer::cinStart() {
+	if (cinStartTime == std::chrono::time_point<std::chrono::steady_clock>()) {
+		cinStartTime = std::chrono::high_resolution_clock::now();
+	}
+}
+
+void Timer::cinEnd() {
+	cinDuration += std::chrono::high_resolution_clock::now() - cinStartTime;
+	cinStartTime = std::chrono::time_point<std::chrono::steady_clock>();
+	middleTime = std::chrono::high_resolution_clock::now();
+}
+
+void Timer::finish() {
+	std::chrono::duration<double> runDuration(std::chrono::high_resolution_clock::now() - startTime - cinDuration);
+	std::ostringstream ostr;
+	ostr << std::fixed << std::setprecision(3) << runDuration.count();
+	std::cout << std::endl << "Futasido: " << ostr.str() << " masodperc" << std::endl << std::endl;
+}
---
+++ OpenCVHungary/helpers.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <opencv2/opencv.hpp>
+#include <chrono>
+
+namespace cv {
+	void imshowResized(const cv::String& winname, const cv::Mat& img);
+}
+
+extern class Timer {
+
+private:
+	const std::chrono::time_point<std::chrono::steady_clock> startTime;
+	std::chrono::time_point<std::chrono::steady_clock> middleTime, cinStartTime;
+	std::chrono::duration<double, std::milli> cinDuration;
+
+public:
+	/**
+	Elindítja az idõmérést.
+	*/
+	Timer();
+
+	/**
+	Kiírja a timer példányosítása, vagy a midTime/cinEnd függvények meghívása óta eltelt idõt.
+	Az egyes részfeladatok részidejének mérésére alkalmas.
+
+	@return Az eltelt idõ milliszekundumban, zárójelben.
+	*/
+	cv::String midTime();
+
+	/**
+	Feljegyzi az idõt az input bekérés elõtt.
+	Egymás utáni, cinEnd nélküli többszöri meghívás esetén az idõt csak az elsõ futáskor jegyzi fel.
+	*/
+	void cinStart();
+
+	/**
+	Input bekérés után megméri a bekérés eleje óta eltelt idõt, és hozzáadja a teljes input idõtartamhoz.
+	Újraindítja a részidõ mérését.
+	*/
+	void cinEnd();
+
+	/**
+	A program teljes futásidejébõl kivonja a teljes input idõtartamot, majd az eredményt kiírja.
+	*/
+	void finish();
+};
